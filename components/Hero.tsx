1212198d346f496ac584cdfbe5f5279ca9339dc3
sharifa-assi <sharifaalassi2001@gmail.com>
2023-11-15 15:18:22 +0200

    camp section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -28,7 +28,7 @@ const Hero = () => {
        </div>
         </div>
         <div className='relative flex flex-1 items-start'>
-            <div className='relative z-20 w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8'>
+            <div className='relative flex z-20 w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8'>
                 <div className='flex flex-col'>
                     
             <div className='flexBetween'>
